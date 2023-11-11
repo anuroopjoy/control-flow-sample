b9ffcf1ac21ec6850d5cbb187f5b5c7dd24aef12
Anuroop <anuroopjoy@gmail.com>
2023-11-11 20:53:37 +0530

    initial

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,14 +1,32 @@
-import { Component } from '@angular/core';
-import { CommonModule } from '@angular/common';
-import { RouterOutlet } from '@angular/router';
+import { NgIf } from '@angular/common';
+import { Component, OnInit } from '@angular/core';
+import { ControlBase } from './controls/control-base';
+import { DataService } from './data.service';
+import { DynamicFormComponent } from './dynamic-form/dynamic-form.component';
+import { MetaService } from './meta.service';
 
 @Component({
   selector: 'app-root',
-  standalone: true,
-  imports: [CommonModule, RouterOutlet],
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
+  styleUrls: ['./app.component.scss'],
+  standalone: true,
+  imports: [NgIf, DynamicFormComponent]
 })
-export class AppComponent {
-  title = 'control-flow-sample';
+export class AppComponent implements OnInit {
+  title = 'dynamic-form-app';
+  meta!: ControlBase[];
+  data!: any;
+  constructor(
+    private metaService: MetaService,
+    private dataService: DataService
+  ) {}
+
+  async ngOnInit() {
+    this.refresh();
+  }
+
+  async refresh() {
+    this.data = await this.dataService.getData();
+    this.meta = (await this.metaService.getMeta()) as ControlBase[];
+  }
 }
---
+++ src/app/control/control.component.ts
@@ -0,0 +1,32 @@
+import {
+  Component,
+  Input,
+  ViewContainerRef,
+} from '@angular/core';
+import { FormGroup } from '@angular/forms';
+import { ControlBase } from '../controls/control-base';
+import { NgSwitch, NgSwitchCase } from '@angular/common';
+import CheckboxComponent from '../controls/checkbox/checkbox.component';
+import LabelComponent from '../controls/label/label.component';
+import TextInputComponent from '../controls/text-input/text-input.component';
+import DropdownComponent from '../controls/dropdown/dropdown.component';
+
+@Component({
+  selector: 'app-control',
+  templateUrl: './control.component.html',
+  styleUrls: ['./control.component.scss'],
+  standalone: true,
+  imports: [
+    NgSwitch,
+    NgSwitchCase,
+    CheckboxComponent,
+    LabelComponent,
+    TextInputComponent,
+    DropdownComponent,
+  ],
+})
+export class ControlComponent {
+  @Input() control!: ControlBase;
+  @Input() form!: FormGroup;
+  placeholder!: ViewContainerRef;
+}
